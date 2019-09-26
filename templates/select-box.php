92bbd8228ff1f3ad11bbc99249a7f24abbc27bb4
Puleeno Nguyen <puleeno@gmail.com>
2019-09-26 08:35:56 +0700

    Added select image box

---
+++ templates/select-box.php
@@ -0,0 +1,9 @@
+<div class="wallery">
+    <div class="select-image-box">
+        <div class="wrapper">
+            <div class="outline">
+                <i class="wallery-upload"></i>
+            </div>
+        </div>
+    </div>
+</div>
