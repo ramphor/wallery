182f2efe5c617c4fbbaed794629501dd1cd22982
Puleeno Nguyen <puleeno@gmail.com>
2019-10-10 00:12:20 +0700

    Added new templates

---
+++ templates/image-list-styles.php
@@ -0,0 +1,16 @@
+<div class="toolbar image-layouts">
+	<h2 class="heading-images">
+		<?php _e( 'Chapter Images', 'wallery' ); ?>
+	</h2>
+	<div class="layouts">
+		<div class="layout layout-list">
+			<span class="wallery-list"></span>
+		</div>
+		<div class="layout layout-thumbnail">
+			<span class="wallery-layout"></span>
+		</div>
+		<?php do_action( 'wallery_layouts' ); ?>
+		<div class="clearfix"></div>
+	</div>
+	<div class="clearfix"></div>
+</div>
