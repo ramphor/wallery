48fef8e447a2cd9742264921a6808123231d7e7c
Puleeno Nguyen <puleeno@gmail.com>
2019-10-10 21:45:15 +0700

    Support choose images for post meta

--- src/Factory/MetaboxFactory.php
+++ src/Factory/MetaboxFactory.php
@@ -26,35 +26,37 @@ class MetaboxFactory extends Factory
         } else {
             delete_post_meta($post_id, $this->wallary_id);
         }
-	}
+    }
 
-	public function getImages($post_id = null) {
-		$image_ids = get_post_meta($post_id, $this->wallary_id, true);
-		if (count($image_ids)) {
-			$images = [];
-			foreach ($image_ids as $image_id) {
-				$image = [
-					'id' => $image_id,
-					'src' => wp_get_attachment_url( $image_id, 'thumbnail' ),
-				];
-				$images[] = $image;
-			}
-			return $images;
-		}
-		return [];
-	}
+    public function getImages($post_id = null)
+    {
+        $image_ids = get_post_meta($post_id, $this->wallary_id, true);
+        if ($image_ids) {
+            $images = [];
+            foreach ($image_ids as $image_id) {
+                $image = [
+                    'id' => $image_id,
+                    'src' => wp_get_attachment_url($image_id, 'thumbnail'),
+                ];
+                $images[] = $image;
+            }
+            return $images;
+        }
+        return [];
+    }
 
     public function render($post)
     {
-		$this->check_id();
-		$wallery_id = $this->wallary_id;
-		$images = $this->getImages($post->ID);
-        wallery_load_template('select-box', compact('wallery_id'));
-	}
+        $this->check_id();
+        $wallery_id = $this->wallary_id;
+        $images = $this->getImages($post->ID);
+        wallery_load_template('select-box', compact('wallery_id', 'images'));
+    }
 
-	public function check_id() {
-		if(empty($this->wallary_id)) {
-			throw new \Exception('Please set ID for wallary can working is correctly.');
-		}
-	}
+    public function check_id()
+    {
+        if (empty($this->wallary_id)) {
+            throw new \Exception('Please set ID for wallary can working is correctly.');
+        }
+    }
 }
--- src/Interfaces/Factory.php
+++ src/Interfaces/Factory.php
@@ -6,7 +6,7 @@ interface Factory
 
 
     public function setId($gallery_id);
-	public function save($object = null);
-	public function getImages($object = null);
+    public function save($object = null);
+    public function getImages($object = null);
     public function render($post);
 }
